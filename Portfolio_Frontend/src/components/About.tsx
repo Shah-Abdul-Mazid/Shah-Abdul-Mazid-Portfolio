f154dbffe057523f5da72fee9e1758aaec179ecd
Shah Abdul Mazid <ezanshah58@gmail.com>
2026-09-09 18:44:17 +0600

    fix: enhance ORCID link with icon and improve accessibility attributes

--- Portfolio_Frontend/src/components/About.tsx
+++ Portfolio_Frontend/src/components/About.tsx
@@ -70,12 +70,18 @@ const About = ({ addToRefs }: { addToRefs: (el: HTMLElement | null) => void }) =
                             >
                                 My Google Scholar
                             </a>
-                            <a 
-                                href="https://orcid.org/0009-0009-6864-5343" 
-                                target="_blank" 
-                                rel="noopener noreferrer"
-                                className="btn-glow"
-                            >
+                            <a
+                            id="cy-effective-orcid-url"
+                            class="underline"
+                            href="https://orcid.org/0009-0009-6864-5343"
+                            target="orcid.widget"
+                            rel="me noopener noreferrer"
+                            style="vertical-align: top">
+                            <img
+                                src="https://orcid.org/sites/default/files/images/orcid_16x16.png"
+                                style="width: 1em; margin-inline-start: 0.5em"
+                                alt="ORCID iD icon"/>
+                    
                                 My ORCID
                             </a>
                             <a 
